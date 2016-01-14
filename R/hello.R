f3d8a08893a04b9d8d6e8fa479c60d4fd2827abd
Ren-Huai Huang <huangrenhuai@gmail.com>
2016-01-14 16:13:10 -0600

    end of the day 1/14/2016

--- R/hello.R
+++ R/hello.R
@@ -14,5 +14,5 @@
 #   Test Package:              'Cmd + Shift + T'
 
 hello <- function() {
-  print("Hello, world! test 2")
+  print("Hello, world! test 3")
 }
---
+++ R/helper.R
@@ -0,0 +1,18 @@
+# Subtract baseline of the the same pin in each cycle
+baseline <- function(dat = dat[-1], index= index, offset = 5) {
+    for (level in (levels(index))) {
+        row_idx = index == level
+        for (col_idx in (names(dat))) {
+            temp = dat[row_idx, ] # slice the data by the row index
+            offset = offset # offset to the start and the end of the baseline
+            start = min(temp$Time) + step_time['prebaseline'] + offset
+            end = start + step_time['baseline'] - 2 * offset
+            # further slice the data to just keep the baseline portion.
+            temp =  dplyr::filter(dat[row_idx, ], Time > start, Time < end)
+            baseline = mean(temp[[col_idx]]);
+            dat[row_idx, col_idx] <- dat[row_idx, col_idx] - baseline
+        }
+    }
+    return(dat)
+
+}
