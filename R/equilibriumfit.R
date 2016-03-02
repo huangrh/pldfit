6dc7e4736a85fda799bca7defb28b4c3d2d3a67c
Ren-Huai Huang <huangrenhuai@gmail.com>
2016-03-02 15:57:01 -0600

    write ssgfit function

--- R/equilibriumfit.R
+++ R/equilibriumfit.R
@@ -1,49 +1,52 @@
+#' Static State SensorGram Data Fitting
+#'
 #' Equilibrium Data Fitting of Protein-Ligand Sensorgram
 #'
 #' @export
-ssgfit <- function(dat,
+ssgfit <- function(datF,
                    concs,
                    start = list(Rmax = 0.5, Kd=1),
-                   width =5,
-                   t2 = 149.5,
+                   width = 5,
+                   t2    = 149.5,
                    model = c("simple1to1", "dimer")
                    )
 {
 
     # data transform
-
-    dat2 <- data.frame(t(dplyr::filter(dat, Time > t2 - width, Time <t2 - 0.5)))
-
-    dat2 <- dplyr::mutate(
-        dat2,
-        mean = apply(dat2, 1, FUN = mean),
-        sd = apply(dat2, 1, FUN=sd)
+    dat <- dplyr::filter(datF, Time > t2 - width, Time <t2 - 0.5)
+    dat <- data.frame(t(dat[!grepl("time", tolower(colnames(dat)))]))
+    #
+    dat <- dplyr::mutate(
+        dat,
+        mean = apply(dat, 1, FUN = mean),
+        sd   = apply(dat, 1, FUN=sd)
     )
 
     # data
-    yData <- dat2$mean
-    xData <- concs
-    list(xData, yData, model)
-}
+    yData <- as.numeric(dat$mean)
+    xData <- as.numeric(concs)
 
-#' Choose A Equilibrium Model.
-#'
-#' Choose a model between "simple1to1" and "dimer" to fit the equilibrium data
-#'
-#' @param model The model symbol. Choose the model bewteen "simple1to1", and "dimer"
-#'
-#' @export
-equilibrium_model <- function(model = c("simple1to1", "dimer")) {
-    #simple1to1 model
+    # Model dispatch
+    #  simple1to1 model in equilibrium
     simple1to1_model <- formula(yData ~ (Rmax * xData)/(Kd + xData))
-
-    # dimer
-    dimer_model <- formula( yData ~
-                                (Rmax * (-Kd + sqrt(Kd^2+8*Kd*xData)))/(3*Kd +sqrt(Kd^2+8*Kd*xData)))
-
-    # cho
-    chosenModel = switch(model[1],
+    #  dimerization in equilibrium
+    dimer_model <-
+        formula( yData ~ (Rmax * (-Kd + sqrt(Kd^2+8*Kd*xData)))/(3*Kd +sqrt(Kd^2+8*Kd*xData)))
+    #  choose a model
+    chosenFormula = switch(model[1],
                          simple1to1 = simple1to1_model,
                          dimer = dimer_model)
-    return(chosenModel)
+
+    ## Fitting
+    fit <- stats::nls(formula = chosenFormula,
+                      start = list(Rmax = 0.5, Kd=1))
+    fit$datF  = data.frame(Conc = xData,
+                           Response = yData,
+                           sd = as.numeric(dat$sd))
+    fit$predict <- data.frame(xData = seq(min(xData), max(xData), len=100 ))
+    fit$predict$yData <- predict(fit, newdata=fit$predict)
+
+    return(fit)
 }
+
+
